70058f29b064313de54747233473ea17886c6f8c
Faithfuloscar <oscarmendez2003@outlook.com>
2024-10-23 19:47:40 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// lecture 2-2
+
+//string variable
+let myVariable = "Hello World!";
+console.log(myVariable);
+
+// change the string variable value
+myVariable = "The value has been changed to the string";
+console.log(myVariable);
+
+//constant variable
+const pi = 3.14 ;
+
+// pi = 7 // Throws an error code
+
+let myNumber = 10;
+
+let result = Math.min(myNumber, pi);
+console.log(result);
